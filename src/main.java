af9bf4cdf9d9d0c9924d7ff9b6f3092680930124
laess <laessig.pauline@aol.com>
2018-06-05 15:40:13 +0200

    1.0.5

--- src/main.java
+++ src/main.java
@@ -1,79 +1,78 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.io.File;
-
-import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import java.io.*;
 
 public class main extends Application {
 
-    Button button;
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage){
         try {
-            // 1. STAGE
-            // Damit setzen wir den Titel unseres Fensters
             primaryStage.setTitle("Dies ist ein Fenster lul");
 
-            // Wir verwenden einen StackPane, dabei handelt es sich um eine Layoutklasse,
-            // welche die Eigenschaften
-            // und Funktionalität des Layouts bestimmen
-            StackPane root = new StackPane();
-
-            // 2.SCENE
-            // Wir legen unsere Scene an, welche innerhalb des StackPanes root ist
-            // und eine Größe von 400 * 400 Pixel hat
-            Scene scene = new Scene(root,600,400);
-
-
-
             MenuBar menuBar = new MenuBar();
-
-            // --- Menu File
             Menu menuOpen = new Menu("Open");
-
-            // --- Menu Edit
             Menu menuSave = new Menu("Save");
-
-            // --- Menu View
             Menu menuExit = new Menu("Exit");
 
+            BorderPane root = new BorderPane();
             menuBar.getMenus().addAll(menuOpen, menuSave, menuExit);
+            root.setTop(menuBar);
 
-            menuBar.setTranslateY(-190);
+            Button button = new Button("Open dis");
+            root.setCenter(button);
 
-            root.getChildren().addAll(menuBar);
+            menuExit.setOnAction(e -> Platform.exit()); //funktioniert nicht
 
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Open Resource File");
-            fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-            File selectedFile = fileChooser.showOpenDialog(primaryStage);
-            if (selectedFile != null) {
-                System.out.println("Erfolgreich");
-            }
+            button.setOnAction(new EventHandler<ActionEvent>(){ //funktioniert nur bei Button, nicht mit menuOpen
+                @Override
+                public void handle(ActionEvent open){
+                    FileChooser fileChooser = new FileChooser();
+                    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+                    fileChooser.getExtensionFilters().add(extFilter);
+                    File file = fileChooser.showOpenDialog(primaryStage);
+                    if(file != null){
+                        System.out.print(file);
+                    }
+                }
+            });
 
-
-            // nun Setzen wir die Scene zu unserem Stage und zeigen ihn an
+            Scene scene = new Scene(root,600,400);
             primaryStage.setScene(scene);
             primaryStage.show();
 
         } catch(Exception e) {
             e.printStackTrace();
         }
-    }
 
-    public static void main(String[] args) {
 
-        // In der Main Methode rufen wir launch auf, welches unser Fenster startet
-        launch(args);
     }
+    /*private String readFile (File file){
+        StringBuilder stringBuffer = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        try{
+            bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+                stringBuffer.append(line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return stringBuffer.toString();
+    }*/
+
 }
