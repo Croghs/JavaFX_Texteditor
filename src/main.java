4bf2528b504722e21af51dbf71d01dcfa2b4ef05
laess <laessig.pauline@aol.com>
2018-06-05 20:23:43 +0200

    1.0.6

--- src/main.java
+++ src/main.java
@@ -2,14 +2,17 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import java.io.*;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class main extends Application {
 
@@ -23,52 +26,85 @@ public class main extends Application {
             primaryStage.setTitle("Dies ist ein Fenster lul");
 
             MenuBar menuBar = new MenuBar();
-            Menu menuOpen = new Menu("Open");
-            Menu menuSave = new Menu("Save");
-            Menu menuExit = new Menu("Exit");
+            Menu menu = new Menu("Menü");
+            MenuItem menuOpen = new MenuItem("Open");
+            MenuItem menuSave = new MenuItem("Save");
+            MenuItem menuExit = new MenuItem("Exit");
+            menu.getItems().addAll(menuOpen,menuSave,menuExit);
+
 
             BorderPane root = new BorderPane();
-            menuBar.getMenus().addAll(menuOpen, menuSave, menuExit);
+            menuBar.getMenus().addAll(menu);
             root.setTop(menuBar);
 
-            Button button = new Button("Open dis");
-            root.setCenter(button);
-
-            menuExit.setOnAction(e -> Platform.exit()); //funktioniert nicht
-
-            button.setOnAction(new EventHandler<ActionEvent>(){ //funktioniert nur bei Button, nicht mit menuOpen
+            menuOpen.setOnAction(new EventHandler<ActionEvent>(){
                 @Override
                 public void handle(ActionEvent open){
                     FileChooser fileChooser = new FileChooser();
                     FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
                     fileChooser.getExtensionFilters().add(extFilter);
-                    File file = fileChooser.showOpenDialog(primaryStage);
+                    File file = fileChooser.showOpenDialog(null);
                     if(file != null){
-                        System.out.print(file);
+                        try{
+                            Files.lines(file.toPath()).forEach(System.out::println);
+                        } catch (IOException ex){
+                            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
                 }
             });
 
-            Scene scene = new Scene(root,600,400);
+            menuSave.setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent save) {
+                    FileChooser fileChooser = new FileChooser();
+                    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+                    fileChooser.getExtensionFilters().add(extFilter);
+                    File file = fileChooser.showSaveDialog(primaryStage);
+                    if(file != null){
+                        SaveFile("blabla", file);
+                    }
+                }
+            });
+
+            menuExit.setOnAction(e -> Platform.exit());
+
+            Scene scene = new Scene(root,400,400);
             primaryStage.setScene(scene);
             primaryStage.show();
 
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
 
+    private void SaveFile(String content, File file){
+        try {
+            FileWriter fileWriter = null;
+
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
-    /*private String readFile (File file){
-        StringBuilder stringBuffer = new StringBuilder();
-        BufferedReader bufferedReader = null;
-        try{
-            bufferedReader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = bufferedReader.readLine()) != null)
-                stringBuffer.append(line);
 
-        } catch (IOException e) {
+     /*private String readFile (File file){
+        StringBuilder stringBuffer = new StringBuilder();
+
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String text;
+            while ((text = bufferedReader.readLine()) != null) {
+                stringBuffer.append(text);
+            }
+            bufferedReader.close();
+        }
+
+        catch (IOException e) {
             e.printStackTrace();
         }
 
